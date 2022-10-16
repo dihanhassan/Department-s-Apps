24797ee78258ee1baf8ebf8c284b5fd19d63a018
Shihab <dihanhassan21@gmail.com>
2022-10-16 21:59:58 +0600

    This is Image Slide

---
+++ app/src/main/java/com/example/imageslide/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.imageslide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageSlider imageSlider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageSlider = (ImageSlider) findViewById(R.id.imgSlider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.img_1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_3, ScaleTypes.FIT));
+        imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+
+    }
+}
