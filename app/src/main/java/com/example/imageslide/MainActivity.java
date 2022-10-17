3e813e6b1fd33c421bf707a929dada927196973d
Shihab <dihanhassan21@gmail.com>
2022-10-18 01:47:06 +0600

    This is font end

--- app/src/main/java/com/example/imageslide/MainActivity.java
+++ app/src/main/java/com/example/imageslide/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.imageslide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.denzcoskun.imageslider.ImageSlider;
@@ -20,12 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.setTitle("ICT INFO");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.meterial_deep_blue)));
+
         imageSlider = (ImageSlider) findViewById(R.id.imgSlider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
         slideModels.add(new SlideModel(R.drawable.img_1, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.img_2, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.img_3, ScaleTypes.FIT));
         imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+
 
     }
 }
